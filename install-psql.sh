7222993b322cdafa8bf30d0d8ee4a316a13aa2d5
Jonah May <119529402+JonahMMay@users.noreply.github.com>
2023-05-11 10:37:42 -0500

    Update install-psql.sh

--- install-psql.sh
+++ install-psql.sh
@@ -8,24 +8,49 @@ do
   if [[ $vbsf_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     ip_valid=true
   else
-    echo "$vbsf_ip is not a valid IP address. Please try again."
+    printf "$vbsf_ip is not a valid IP address. Please try again."
   fi
 done
 
 # Update package information before installation
-echo 'Updating package information...'
+printf 'Updating package information...'
 apt-get update
 
 # Install PostgreSQL
-echo 'Installing PostgreSQL...'
+printf 'Installing PostgreSQL...'
 apt install postgresql -y
 
 # Check installed version
 PSQL_MAJOR_VER = postgres -V | egrep -o '[0-9]{1,}'
 
 # Enable remote connections
-echo 'Editing postgresql.conf for remote connections...'
+printf 'Editing postgresql.conf for remote connections...'
 sed -i 's/#listen_addresses=\'localhost\'/listen_addresses=\'*\'/' /etc/postgresql/$PSQL_MAJOR_VER/main/postgresql.conf 
 
-echo 'Editing pg_hba.conf for remote connections...'
+printf 'Editing pg_hba.conf for remote connections...'
 sed -i 's/host    all             all             127.0.0.1/32            md5/host    all             all             '$vbsf_ip'/32            md5/' /etc/postgresql/$PSQL_MAJOR_VER/main/postgresql.conf
+
+# Restart service so changes can take effect
+printf 'Restarting PostgreSQL service to apply changes...'
+service postgresql restart
+
+# Switch to postgres user to create account for Veeam to use for access
+printf 'Switching to postgres user...'
+su - postgres
+
+printf 'Creating Veeam database user...'
+createuser -l -d veeambackupsf
+
+# Generate random password
+$veeam_password = gpg --gen-random --armor 1 14
+
+# Apply password to account
+printf veeambackupsf:$veeam_password | chpasswd
+
+# TODO: Output the password to the console for the user to copy
+printf "\n\n\nPlease make sure to copy the following lines as they will NOT be saved and are needed by Veeam."
+printf "Username: veeambackupsf"
+printf "Password: $veeam_password"
+
+printf "\n\n\nInstallation complete!"
+exit
